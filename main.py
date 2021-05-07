0ddc3791ca14edc8dd78c0e62658e46e4fc5f799
MarchenkoVlad <vlad.u.m.ru@gmail.com>
2021-05-07 17:34:40 +0300

    добавил получение и вывод данных с парсера

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import telebot
 import config
-
+import parse
+import json
 
 bot = telebot.TeleBot(config.token)
 
+def get_news():
+    data = parse.parse()
+    title_news = [i['title'] for i in data]
+    return title_news
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(commands=['start'])
+def send_news(message):
+    title_news = get_news()
+    print(title_news)
+    title_news = [f'{i+1}. {j} \n' for i, j in enumerate(title_news)]
+    bot.reply_to(message, ''.join(title_news))
+
 
 if __name__ =='__main__':
     bot.infinity_polling()
